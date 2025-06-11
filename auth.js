e80417ffa64eae44827372f6fd99f1433f494293
Ankush22004 <akushS0192@gmail.com>
2025-06-11 19:29:56 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,22 @@
+// auth.js
+
+// TODO: Replace with your Firebase config and enable email/password login
+
+/*
+firebase.initializeApp({
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+});
+
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    alert(`👋 Welcome ${user.email}`);
+  } else {
+    const email = prompt("Email:");
+    const password = prompt("Password:");
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(err => alert("Login failed: " + err.message));
+  }
+});
+*/
